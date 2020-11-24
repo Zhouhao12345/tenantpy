42b9e05a37fd5d7643dfe6afe90dbf5bce4a3df0
alex.zhou <alex.zhou@gllue.com>
2020-11-24 14:24:50 +0800

    [ADD] first commit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def description():
 
 setup(
     name="tenantpy",
-    version="1.1.5",
+    version="1.0.0",
     author="zhouhao",
     python_requires='>=3.7.0',
     author_email="zhouhao19931002@hotmail.com",
